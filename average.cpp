b5a420924597e680d8200dc3f303278f295685f7
YoYaYee <EDEN555007@gmail.com>
2025-02-18 14:49:20 +0700

    average_complete.cpp

--- average.cpp
+++ average.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
 
+int main(int argc, char* argv[]){
+    float avg;
+    float a[argc] = {};
+    for(int i; i < argc; i++){
+        a[i] = atoi(argv[i]);
+    }
+    for(int j; j < argc; j++){
+        avg += a[j];
+    }
+    if(argc <= 1)
+    cout << "Please input numbers to find average." << endl;
+    else {
+    cout << "---------------------------------" << endl;
+    cout << "Average of " << argc-1 << " numbers = " << avg/(argc-1) << endl;
+    cout << "---------------------------------" << endl;
+    }
+
+}
